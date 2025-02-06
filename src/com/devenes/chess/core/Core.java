d5fbfd0da213ad2c2e1ffb371fc932bfc2551246
Enes Türker <Enes.turker446@hotmail.com>
2025-02-06 22:06:16 +0300

    updated makeMove method

--- src/com/devenes/chess/core/Core.java
+++ src/com/devenes/chess/core/Core.java
@@ -5,7 +5,9 @@ import com.devenes.chess.pieces.*;
 
 import javax.swing.*;
 import java.awt.*;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+import java.util.Map;
 
 public class Core {
 
@@ -151,174 +153,59 @@ public class Core {
         frame.revalidate();
     }
 
-    public void makeMove() {
+    public void makeMove() throws NoSuchFieldException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Map<Class<?>, Integer> pieceValues = Map.of(
+                Pawn.class, 6,
+                Bishop.class, 5,
+                Knight.class, 4,
+                Rook.class, 3,
+                Queen.class, 2,
+                King.class, 1
+        );
+
         if (Core.selectedPiece != null && Core.targetSquare != null) {
             // Move to an empty square
-            if (Core.selectedPiece instanceof Pawn pawn) {
-                Core.board[pawn.row][pawn.column] = 0;
-                String[] split = Core.targetSquare.split(",");
-                int x = Converter.columnToX(Integer.parseInt(split[1]));
-                int y = Converter.rowToY(Integer.parseInt(split[0]));
-                pawn.x = x;
-                pawn.y = y;
-                pawn.setLocation(x, y);
-                pawn.row = Converter.yToRow(y);
-                pawn.column = Converter.xToColumn(x);
-                Core.board[pawn.row][pawn.column] = pawn.color.equals("white") ? 6 : 12;
-            } else if (Core.selectedPiece instanceof Bishop bishop) {
-                Core.board[bishop.row][bishop.column] = 0;
-                String[] split = Core.targetSquare.split(",");
-                int x = Converter.columnToX(Integer.parseInt(split[1]));
-                int y = Converter.rowToY(Integer.parseInt(split[0]));
-                bishop.x = x;
-                bishop.y = y;
-                bishop.setLocation(x, y);
-                bishop.row = Converter.yToRow(y);
-                bishop.column = Converter.xToColumn(x);
-                Core.board[bishop.row][bishop.column] = bishop.color.equals("white") ? 5 : 11;
-            } else if (Core.selectedPiece instanceof Knight knight) {
-                Core.board[knight.row][knight.column] = 0;
-                String[] split = Core.targetSquare.split(",");
-                int x = Converter.columnToX(Integer.parseInt(split[1]));
-                int y = Converter.rowToY(Integer.parseInt(split[0]));
-                knight.x = x;
-                knight.y = y;
-                knight.setLocation(x, y);
-                knight.row = Converter.yToRow(y);
-                knight.column = Converter.xToColumn(x);
-                Core.board[knight.row][knight.column] = knight.color.equals("white") ? 4 : 10;
-            } else if (Core.selectedPiece instanceof Rook rook) {
-                Core.board[rook.row][rook.column] = 0;
-                String[] split = Core.targetSquare.split(",");
-                int x = Converter.columnToX(Integer.parseInt(split[1]));
-                int y = Converter.rowToY(Integer.parseInt(split[0]));
-                rook.x = x;
-                rook.y = y;
-                rook.setLocation(x, y);
-                rook.row = Converter.yToRow(y);
-                rook.column = Converter.xToColumn(x);
-                Core.board[rook.row][rook.column] = rook.color.equals("white") ? 3 : 9;
-            } else if (Core.selectedPiece instanceof Queen queen) {
-                Core.board[queen.row][queen.column] = 0;
-                String[] split = Core.targetSquare.split(",");
-                int x = Converter.columnToX(Integer.parseInt(split[1]));
-                int y = Converter.rowToY(Integer.parseInt(split[0]));
-                queen.x = x;
-                queen.y = y;
-                queen.setLocation(x, y);
-                queen.row = Converter.yToRow(y);
-                queen.column = Converter.xToColumn(x);
-                Core.board[queen.row][queen.column] = queen.color.equals("white") ? 2 : 8;
-            } else if (Core.selectedPiece instanceof King king) {
-                Core.board[king.row][king.column] = 0;
-                String[] split = Core.targetSquare.split(",");
-                int x = Converter.columnToX(Integer.parseInt(split[1]));
-                int y = Converter.rowToY(Integer.parseInt(split[0]));
-                king.x = x;
-                king.y = y;
-                king.setLocation(x, y);
-                king.row = Converter.yToRow(y);
-                king.column = Converter.xToColumn(x);
-                Core.board[king.row][king.column] = king.color.equals("white") ? 1 : 7;
-            }
+            Object selectedP = Core.selectedPiece;
+            int selectedPieceRow = (int) selectedP.getClass().getField("row").get(selectedP);
+            int selectedPieceColumn = (int) selectedP.getClass().getField("column").get(selectedP);
+            Core.board[selectedPieceRow][selectedPieceColumn] = 0;
+            String[] split = Core.targetSquare.split(",");
+            int x = Converter.columnToX(Integer.parseInt(split[1]));
+            int y = Converter.rowToY(Integer.parseInt(split[0]));
+            selectedP.getClass().getField("x").set(selectedP, x);
+            selectedP.getClass().getField("y").set(selectedP, y);
+            selectedP.getClass().getMethod("setLocation", int.class, int.class).invoke(selectedP, x, y);
+            selectedP.getClass().getField("row").set(selectedP, Converter.yToRow(y));
+            selectedP.getClass().getField("column").set(selectedP, Converter.xToColumn(x));
+            int row = (int) selectedP.getClass().getField("row").get(selectedP);
+            int column = (int) selectedP.getClass().getField("column").get(selectedP);
 
+            int baseValue = pieceValues.getOrDefault(selectedP.getClass(), 0);
+            String color = (String) selectedP.getClass().getField("color").get(selectedP);
+            Core.board[row][column] = color.equals("white") ? baseValue : baseValue + 6;
 
         } else if (Core.selectedPiece != null && Core.targetPiece != null) {
             // Taking a piece
-            int targetX = 0, targetY = 0, targetRow = 0, targetColumn = 0;
-            if (Core.targetPiece instanceof Pawn pawn) {
-                targetX = pawn.x;
-                targetY = pawn.y;
-                targetRow = pawn.row;
-                targetColumn = pawn.column;
-                Core.pieces.remove(pawn);
-            } else if (Core.targetPiece instanceof Bishop bishop) {
-                targetX = bishop.x;
-                targetY = bishop.y;
-                targetRow = bishop.row;
-                targetColumn = bishop.column;
-                Core.pieces.remove(bishop);
-            } else if (Core.targetPiece instanceof Knight knight) {
-                targetX = knight.x;
-                targetY = knight.y;
-                targetRow = knight.row;
-                targetColumn = knight.column;
-                Core.pieces.remove(knight);
-            }
-            else if (Core.targetPiece instanceof Rook rook) {
-                targetX = rook.x;
-                targetY = rook.y;
-                targetRow = rook.row;
-                targetColumn = rook.column;
-                Core.pieces.remove(rook);
-            } else if (Core.targetPiece instanceof Queen queen) {
-                targetX = queen.x;
-                targetY = queen.y;
-                targetRow = queen.row;
-                targetColumn = queen.column;
-                Core.pieces.remove(queen);
-            } else if (Core.targetPiece instanceof King king) {
-                targetX = king.x;
-                targetY = king.y;
-                targetRow = king.row;
-                targetColumn = king.column;
-                Core.pieces.remove(king);
-            }
+            Object targetP = Core.targetPiece;
+            int targetX = (int) targetP.getClass().getField("x").get(targetP);
+            int targetY = (int) targetP.getClass().getField("y").get(targetP);
+            int targetRow = (int) targetP.getClass().getField("row").get(targetP);
+            int targetColumn = (int) targetP.getClass().getField("column").get(targetP);
+            Core.pieces.remove(targetP);
 
-            if (Core.selectedPiece instanceof Pawn pawn) {
-                Core.board[pawn.row][pawn.column] = 0;
-                pawn.x = targetX;
-                pawn.y = targetY;
-                pawn.row = targetRow;
-                pawn.column = targetColumn;
-                pawn.setLocation(pawn.x, pawn.y);
-                Core.board[targetRow][targetColumn] = pawn.color.equals("white") ? 6 : 12;
-            } else if (Core.selectedPiece instanceof Bishop bishop) {
-                Core.board[bishop.row][bishop.column] = 0;
-                bishop.x = targetX;
-                bishop.y = targetY;
-                bishop.row = targetRow;
-                bishop.column = targetColumn;
-                bishop.setLocation(bishop.x, bishop.y);
-                Core.board[targetRow][targetColumn] = bishop.color.equals("white") ? 5 : 11;
-            } else if (Core.selectedPiece instanceof Knight knight) {
-                Core.board[knight.row][knight.column] = 0;
-                knight.x = targetX;
-                knight.y = targetY;
-                knight.row = targetRow;
-                knight.column = targetColumn;
-                knight.setLocation(knight.x, knight.y);
-                Core.board[targetRow][targetColumn] = knight.color.equals("white") ? 4 : 10;
-            }
-            else if (Core.selectedPiece instanceof Rook rook) {
-                Core.board[rook.row][rook.column] = 0;
-                rook.x = targetX;
-                rook.y = targetY;
-                rook.row = targetRow;
-                rook.column = targetColumn;
-                rook.setLocation(rook.x, rook.y);
-                Core.board[targetRow][targetColumn] = rook.color.equals("white") ? 3 : 9;
+            Object selectedP = Core.selectedPiece;
+            int selectedPieceRow = (int) selectedP.getClass().getField("row").get(selectedP);
+            int selectedPieceColumn = (int) selectedP.getClass().getField("column").get(selectedP);
+            Core.board[selectedPieceRow][selectedPieceColumn] = 0;
+            selectedP.getClass().getField("x").set(selectedP, targetX);
+            selectedP.getClass().getField("y").set(selectedP, targetY);
+            selectedP.getClass().getField("row").set(selectedP, targetRow);
+            selectedP.getClass().getField("column").set(selectedP, targetColumn);
+            selectedP.getClass().getMethod("setLocation", int.class, int.class).invoke(selectedP, targetX, targetY);
 
-            } else if (Core.selectedPiece instanceof Queen queen) {
-                Core.board[queen.row][queen.column] = 0;
-                queen.x = targetX;
-                queen.y = targetY;
-                queen.row = targetRow;
-                queen.column = targetColumn;
-                queen.setLocation(queen.x, queen.y);
-                Core.board[targetRow][targetColumn] = queen.color.equals("white") ? 2 : 8;
-            } else if (Core.selectedPiece instanceof King king) {
-                Core.board[king.row][king.column] = 0;
-                king.x = targetX;
-                king.y = targetY;
-                king.row = targetRow;
-                king.column = targetColumn;
-                king.setLocation(king.x, king.y);
-                Core.board[targetRow][targetColumn] = king.color.equals("white") ? 1 : 7;
-            }
-
-            Core.pieces.remove(Core.targetPiece);
-
+            int baseValue = pieceValues.getOrDefault(selectedP.getClass(), 0);
+            String color = (String) selectedP.getClass().getField("color").get(selectedP);
+            Core.board[targetRow][targetColumn] = color.equals("white") ? baseValue : baseValue + 6;
 
         } else return;
 
--- src/com/devenes/chess/listeners/MouseListener.java
+++ src/com/devenes/chess/listeners/MouseListener.java
@@ -1,9 +1,6 @@
 package com.devenes.chess.listeners;
 
 import com.devenes.chess.Converter;
-import com.devenes.chess.core.Core;
-import com.devenes.chess.core.Game;
-import com.devenes.chess.pieces.*;
 
 import java.awt.event.MouseEvent;
 
@@ -21,19 +18,13 @@ public class MouseListener implements java.awt.event.MouseListener {
         if (selectedPiece != null) {
             targetSquare = Converter.xToColumn(e.getY()) + "," + Converter.yToRow(e.getX());
             if (possibleMoves.contains(targetSquare)) {
-                if (Core.selectedPiece instanceof Pawn pawn) {
-                    pawn.makeMove();
-                } else if (Core.selectedPiece instanceof Bishop bishop) {
-                    bishop.makeMove();
-                } else if (Core.selectedPiece instanceof Knight knight) {
-                    knight.makeMove();
-                } else if (Core.selectedPiece instanceof Rook rook) {
-                    rook.makeMove();
-                } else if (Core.selectedPiece instanceof Queen queen) {
-                    queen.makeMove();
-                } else if (Core.selectedPiece instanceof King king) {
-                    king.makeMove();
+                Object selectedP = selectedPiece;
+                try {
+                    selectedP.getClass().getMethod("makeMove").invoke(selectedP);
+                } catch (Exception ex) {
+                    throw new RuntimeException(ex);
                 }
+
             } else {
                 selectedPiece = null;
                 targetSquare = null;
